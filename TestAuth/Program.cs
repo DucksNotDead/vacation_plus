0cbf17bb66ad1dfc8347189498f6d6441d8d957c
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-03-26 01:07:01 +0800

    Added /login and /auth (without httpcontext)

--- TestAuth/Program.cs
+++ TestAuth/Program.cs
@@ -8,7 +8,69 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-List<User> Users = new List<User>() { new User("user1", "123"), new User("user2", "1234") };
+List<User> Users = new List<User>();
+
+// ----- Test data -----
+Users = new List<User>();
+User user = new User();
+user.Id = 1;
+user.Mail = "testuser@gmail.com";
+user.Number = 1;
+user.Fio = "Фамилия1 Имя1 Отчество1";
+user.ShortFio = "Фамилия1 И.О.";
+user.Oranization = "Организация1";
+user.JobTitle = "Разработчик";
+user.Control = "Контроль разработчика";
+user.DepartmentId = 1;
+user.Days = 61;
+user.DaysOff = 4;
+user.Password = "123";
+Users.Add(user);
+
+user = new User();
+user.Id = 2;
+user.Mail = "usertest@gmail.com";
+user.Number = 2;
+user.Fio = "Фамилия2 Имя2 Отчество2";
+user.ShortFio = "Фамилия2 И.О.";
+user.Oranization = "Организация1";
+user.JobTitle = "Дизайнер";
+user.Control = "Контроль дизайнера";
+user.DepartmentId = 2;
+user.Days = 240;
+user.DaysOff = 20;
+user.Password = "123";
+Users.Add(user);
+
+user = new User();
+user.Id = 3;
+user.Mail = "ttteeesssttt@yandex.ru";
+user.Number = 3;
+user.Fio = "Фамилия3 Имя3 Отчество3";
+user.ShortFio = "Фамилия3 И.О.";
+user.Oranization = "Организация2";
+user.JobTitle = "Разработчик";
+user.Control = "Контроль разработчика";
+user.DepartmentId = 3;
+user.Days = 10;
+user.DaysOff = 0;
+user.Password = "123";
+Users.Add(user);
+
+user = new User();
+user.Id = 4;
+user.Mail = "mblksmbkl@mail.ru";
+user.Number = 4;
+user.Fio = "Фамилия31 Имя42 Отчество53";
+user.ShortFio = "Фамилия31 И.О.";
+user.Oranization = "Организация1";
+user.JobTitle = "Разработчик";
+user.Control = "Контроль разработчика";
+user.DepartmentId = 1;
+user.Days = 628;
+user.DaysOff = 44;
+Users.Add(user);
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,73 +80,93 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)    //
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = false,                  // указывает, будет ли валидироваться издатель при валидации токена
-            //ValidIssuer = AuthOptions.ISSUER,       // строка, представляющая издателя
             ValidateAudience = false,                // будет ли валидироваться потребитель токена
-            //ValidAudience = AuthOptions.AUDIENCE,   // установка потребителя токена
             ValidateLifetime = true,                // будет ли валидироваться время существования
             IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(), // установка ключа безопасности
             ValidateIssuerSigningKey = true,        // валидация ключа безопасности
         };
-    });                            
+    });   
+
 builder.Services.AddAuthorization(); // добавление сервисов авторизации
 
 var app = builder.Build();
 
 // Для фронта
-app.UseDefaultFiles();
-app.UseStaticFiles();
+// app.UseDefaultFiles();
+// app.UseStaticFiles();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapPost("/login", (User loginData) =>
+app.MapPost("/login", (LoginData loginData) =>
 {
-    User? user = Users.FirstOrDefault(u => (u.Name == loginData.Name && u.Password == loginData.Password));
+    User? user = Users.FirstOrDefault(u => (u.Mail == loginData.Mail && u.Password == loginData.Password));
     if (user == null)
         return Results.Unauthorized();
 
-    //var claims = new List<Claim> { new Claim("UserName", loginData.Name) }; // Если надо данные в токен
+    var claims = new List<Claim>() { new Claim("UserId", user.Id.ToString()) };
     var jwt = new JwtSecurityToken(
-            issuer: AuthOptions.ISSUER,
-            audience: AuthOptions.AUDIENCE,
-            //claims: claims,
-            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)), // время действия 2 минуты
-            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+        claims: claims,
+        expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(AuthOptions.TokenLifeTime)),
+        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
 
     var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
     var response = new
     {
+        userdata = (UserData)user,
         access_token = encodedJwt,
-        username = user.Name
     };
 
     return Results.Json(response);
 });
 
-app.Map("/data", [Authorize] () => new { message = "Hello World!" }); // Только для авторизованного пользователя
+app.MapPost("/auth", (string encodedJwt) =>
+{
+    var handler = new JwtSecurityTokenHandler();
+    int UserId = int.Parse(handler.ReadJwtToken(encodedJwt).Claims.First(c => c.Type == "UserId").Value);
+
+    User? user = Users.FirstOrDefault(u => u.Id == UserId);
+    var response = new
+    {
+        userdata = (UserData?)user
+    };
+
+    return Results.Json(response);
+});
 
 app.Run();
 
 // Можно избавиться
 public class AuthOptions
 {
-    public const string ISSUER = "MyAuthServer";    // издатель токена
-    public const string AUDIENCE = "MyAuthClient";  // потребитель токена
     const string KEY = "supersecretkey00supersecretkey00"; // ключ для шифрации [по идее его надо хранить в другом месте]
+    public const int TokenLifeTime = 5;
     public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
         new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
 }
 
-// Расширить
-public class User
+public class UserData
 {
-    public string Name { get; set; }
+    public int Id { get; set; }
+    public string Mail { get; set; }
+    public int Number { get; set; }
+    public string Fio { get; set; }
+    public string ShortFio { get; set; }
+    public string Oranization { get; set; }
+    public string JobTitle { get; set; }
+    public string Control { get; set; }
+    public int DepartmentId { get; set; }
+    public int Days { get; set; }
+    public int DaysOff { get; set; }
+}
+public class User : UserData
+{
     public string Password { get; set; }
+}
 
-    public User(string name, string password)
-    {
-        Name = name;
-        Password = password;
-    }
+public class LoginData
+{
+    public string Mail { get; set; }
+    public string Password { get; set; }
 }
